aefbe6f05bd492a97aaacc3723f3d899d17fbd1c
ttoanttran <ttoantran603@gmail.com>
2023-04-05 16:01:32 -0700

    Added an button and imageView to encrypt page to allow user to upload image from their gallery (downloads)

--- app/src/main/java/com/example/myapplication/encrypt_page.kt
+++ app/src/main/java/com/example/myapplication/encrypt_page.kt
@@ -1,12 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 
 class encrypt_page : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encrypt_page)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        button = findViewById(R.id.upload_button)
+        imageView = findViewById(R.id.e_image)
+
+        val galleryImage = registerForActivityResult(ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                imageView.setImageURI(it)
+            })
+
+        button.setOnClickListener {
+            galleryImage.launch("image/*")
+        }
     }
 }
