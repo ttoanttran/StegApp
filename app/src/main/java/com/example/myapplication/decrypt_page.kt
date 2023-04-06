7df1f5e9d9e33d82c9f9228f3da2ebb6a5b132c8
ttoanttran <ttoantran603@gmail.com>
2023-04-06 14:55:30 -0700

    Added image gallery upload button in decrypt page.

--- app/src/main/java/com/example/myapplication/decrypt_page.kt
+++ app/src/main/java/com/example/myapplication/decrypt_page.kt
@@ -2,11 +2,32 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 
 class decrypt_page : AppCompatActivity() {
+
+    private lateinit var button: Button
+    private lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decrypt_page)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        button = findViewById(R.id.upload_button)
+        imageView = findViewById(R.id.d_image)
+
+        val galleryImage = registerForActivityResult(ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                imageView.setImageURI(it)
+            })
+
+        button.setOnClickListener {
+            galleryImage.launch("image/*")
+        }
+
     }
 }
