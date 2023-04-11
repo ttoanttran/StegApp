4f07170a26cf2ef36b1ad3d15d5cbc0685c2ac47
ttoanttran <ttoantran603@gmail.com>
2023-04-11 00:03:32 -0700

    Added a encrypted/decrypted notification text on button click

--- app/src/main/java/com/example/myapplication/decrypt_page.kt
+++ app/src/main/java/com/example/myapplication/decrypt_page.kt
@@ -2,8 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 
@@ -11,6 +13,8 @@ class decrypt_page : AppCompatActivity() {
 
     private lateinit var button: Button
     private lateinit var imageView: ImageView
+    private lateinit var status_text: TextView
+    private lateinit var decrypt_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +31,13 @@ class decrypt_page : AppCompatActivity() {
 
         button.setOnClickListener {
             galleryImage.launch("image/*")
+        }
+
+        status_text = findViewById(R.id.d_alert)
+        decrypt_button = findViewById(R.id.d_button)
+
+        decrypt_button.setOnClickListener {
+            status_text.visibility = View.VISIBLE
         }
 
     }
--- app/src/main/java/com/example/myapplication/encrypt_page.kt
+++ app/src/main/java/com/example/myapplication/encrypt_page.kt
@@ -2,22 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 
 class encrypt_page : AppCompatActivity() {
 
-    private lateinit var button: Button
+    private lateinit var imagebutton: Button
     private lateinit var imageView: ImageView
+    private lateinit var status_text: TextView
+    private lateinit var encrypt_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encrypt_page)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        button = findViewById(R.id.upload_button)
+        imagebutton = findViewById(R.id.upload_button)
         imageView = findViewById(R.id.e_image)
 
         val galleryImage = registerForActivityResult(ActivityResultContracts.GetContent(),
@@ -25,8 +29,19 @@ class encrypt_page : AppCompatActivity() {
                 imageView.setImageURI(it)
             })
 
-        button.setOnClickListener {
+        imagebutton.setOnClickListener {
             galleryImage.launch("image/*")
         }
+
+        // Status visibility
+        status_text = findViewById(R.id.e_alert)
+        encrypt_button = findViewById(R.id.e_button)
+
+        encrypt_button.setOnClickListener {
+            status_text.visibility = View.VISIBLE
+        }
+
+
+
     }
 }
