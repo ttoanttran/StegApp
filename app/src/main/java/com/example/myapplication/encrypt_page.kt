ad6cf664cb23e74fd410c0b41f7ea9d8b7bca827
ttoanttran <ttoantran603@gmail.com>
2023-04-16 01:25:10 -0700

    Updated encode and decode

--- app/src/main/java/com/example/myapplication/encrypt_page.kt
+++ app/src/main/java/com/example/myapplication/encrypt_page.kt
@@ -1,11 +1,14 @@
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -21,6 +24,7 @@ class encrypt_page : AppCompatActivity() {
     private lateinit var imageView: ImageView
     private lateinit var status_text: TextView
     private lateinit var encrypt_button: Button
+    private lateinit var download_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,15 +47,31 @@ class encrypt_page : AppCompatActivity() {
         status_text = findViewById(R.id.e_alert)
         encrypt_button = findViewById(R.id.e_button)
 
+        // val encodedImage: Bitmap? = null
+
         encrypt_button.setOnClickListener {
             val messagebox: TextInputEditText = findViewById(R.id.e_messagebox)
-            val passbox: TextInputEditText = findViewById(R.id.passbox)
-            val encodedImage = encodeImage(imageView, messagebox, passbox)
+
+            val encodedImage = encodeImage(imageView, messagebox)
+
+            val m = getPixel(encodedImage)
+            println(m.joinToString(""))
+
+
+
             status_text.visibility = View.VISIBLE
 
 
         }
+
+        download_button = findViewById(R.id.download_button)
+
+        download_button.setOnClickListener {
+
+        }
+
     }
+
 
     private fun convertToBinary(messageInput: String): String {
         val bytes = messageInput.toByteArray()
@@ -67,7 +87,8 @@ class encrypt_page : AppCompatActivity() {
         return binaryString.toString()
     }
 
-    private fun encodeBit(color: Int, bit: Char): Int {
+    // Function to encode one bit of the message in the least significant bit of a color channel
+    fun encodeBit(color: Int, bit: Char): Int {
         return if (bit == '0') {
             color and 0xfe
         } else {
@@ -75,81 +96,113 @@ class encrypt_page : AppCompatActivity() {
         }
     }
 
+    fun getPixel(bitmap: Bitmap): List<Int> {
+        var length = 10
+        var index = 0
+        val pixelLSBs = mutableListOf<Int>()
+        for (y in 0 until bitmap.height) {
+            for (x in 0 until bitmap.width) {
+                val pixel = bitmap.getPixel(x, y)
+                val r = Color.red(pixel) and 1
+                val g = Color.red(pixel) and 1
+                val b = Color.red(pixel) and 1
+                pixelLSBs.add(r)
+                pixelLSBs.add(g)
+                pixelLSBs.add(b)
+                index += 1
+                if (index == length) {
+                    break
+                }
+            }
+            if (index >= length) {
+                break
+            }
+        }
+        return pixelLSBs
+    }
+
+
+
     private fun encodeImage(
         image: ImageView,
-        message: TextInputEditText,
-        password: TextInputEditText
+        message: TextInputEditText
     ): Bitmap {
         //turn message and password into a string first
         val messageString: String = message.text.toString()
-        val passwordString: String = password.text.toString()
 
         // combine password and message and then convert to binary
-        val messageAndPassword = messageString + passwordString
-        val binaryMessage = convertToBinary(messageAndPassword)
+        val binaryMessage = convertToBinary(messageString)
 
         println(binaryMessage)
 
         // turn imageView into a bitmap
         val drawable = image.drawable
-        val bitmap = Bitmap.createBitmap(
-            drawable.intrinsicWidth,
-            drawable.intrinsicHeight,
-            Bitmap.Config.RGB_565
-        )
-        val canvas = Canvas(bitmap)
-        image.draw(canvas)
+        val bitmapDrawable = drawable as BitmapDrawable
+        val bitmap = bitmapDrawable.bitmap
+
+        //val bitmap = Bitmap.createBitmap(oldbitmap.width, oldbitmap.height, oldbitmap.config)
+        //val canvas = Canvas(bitmap)
+        //canvas.drawBitmap(oldbitmap, 0f, 0f, null)
 
         print("Width: ")
         println(bitmap.width)
         print("height: ")
         println(bitmap.height)
 
+        val m = getPixel(bitmap)
+        println(m.joinToString(""))
+
+        var messagelength = binaryMessage.length
+        val lengthBits = Integer.toBinaryString(messagelength).padStart(32, '0')
+
+        val encodedMessage = lengthBits + binaryMessage
+
+        println(messagelength)
+        println(lengthBits)
+
         val maxLength = bitmap.width * bitmap.height * 3
-        if (binaryMessage.length > maxLength) {
+        if (encodedMessage.length > maxLength) {
             throw IllegalArgumentException("Message and password are too long")
         }
 
-        println("testing")
-
-        var length = messageString.length + passwordString.length
-        val lengthBits = Integer.toBinaryString(length).padStart(16, '0')
-        var index = 0
-
-        println("testing2")
-
         //encode message
+        var newLsb = Integer.parseInt(encodedMessage, 2)
         val encodedBitmap = bitmap.copy(bitmap.config, true)
+        var bitIndex = 0
         for (y in 0 until bitmap.height) {
             for (x in 0 until bitmap.width) {
-                // check if we have encoded the length of message and password
-                if (index < lengthBits.length) {
-                    val pixel = bitmap.getPixel(x, y)
-                    val red = encodeBit(Color.red(pixel), lengthBits[index])
-                    val green = encodeBit(Color.green(pixel), lengthBits[index + 1])
-                    val blue = encodeBit(Color.blue(pixel), '0')
-                    val encodePixel = Color.rgb(red, green, blue)
-                    encodedBitmap.setPixel(x, y, encodePixel)
-                    index += 2
-                } else {
-                    val pixel = bitmap.getPixel(x, y)
-                    if (index < binaryMessage.length) {
-                        val red = encodeBit(Color.red(pixel), binaryMessage[index])
-                        val green = encodeBit(
-                            Color.green(pixel),
-                            binaryMessage.getOrElse(index + 1) { '0' })
-                        val blue =
-                            encodeBit(Color.blue(pixel), binaryMessage.getOrElse(index + 2) { '0' })
-                        val encodePixel = Color.rgb(red, green, blue)
-                        encodedBitmap.setPixel(x, y, encodePixel)
-                        index += 3
-                    } else {
-                        encodedBitmap.setPixel(x, y, bitmap.getPixel(x, y))
-                    }
+                if (bitIndex >= encodedMessage.length) {
+                    break
                 }
+                val pixel = bitmap.getPixel(x, y)
+                val red = Color.red(pixel)
+                val green = Color.green(pixel)
+                val blue = Color.blue(pixel)
+                val newRed = (red and 0xFE) or ((newLsb shr 7) and 0x01)
+                val newGreen = (green and 0xFE) or ((newLsb shr 6) and 0x01)
+                val newBlue = (blue and 0xFE) or ((newLsb shr 5) and 0x01)
+                val newPixel = Color.rgb(newRed, newGreen, newBlue)
+                encodedBitmap.setPixel(x, y, newPixel)
+                newLsb = newLsb shl 3
+                bitIndex += 3
+            }
+            if (bitIndex >= encodedMessage.length) {
+                break
             }
         }
+
         return encodedBitmap
     }
+
+
+
+
+    // Function to convert a binary string to a plain text string
+    fun fromBinaryString(binaryString: String): String {
+        val bytes = binaryString.chunked(8).map { Integer.parseInt(it, 2).toByte() }.toByteArray()
+        return String(bytes, Charsets.UTF_8)
+    }
+
+
 }
 
